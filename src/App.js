1df3e3bd24232291a89cbab8d355abdd1e44ef9f
Jakob Schödl <jschoedl04@gmail.com>
2022-01-14 21:40:10 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Player from "./Player";
 
 
 function App() {
     return (
-        <Player address="ban_3mppwzuwc3m586i6pdegsyon76hxnm5pch4jwenb97zamjt7b9o1muadwnp4"/>
+        // <Player address="ban_3mppwzuwc3m586i6pdegsyon76hxnm5pch4jwenb97zamjt7b9o1muadwnp4"/>
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Player
+                        address="ban_3mppwzuwc3m586i6pdegsyon76hxnm5pch4jwenb97zamjt7b9o1muadwnp4"/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
