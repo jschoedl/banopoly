11037e898555a539d95d2405adb8b13aec23aedd
Jakob Schödl <jschoedl04@gmail.com>
2022-01-22 12:11:31 +0100

    style game layout

--- components/Account.js
+++ components/Account.js
@@ -3,7 +3,10 @@ import Balance from "./Balance";
 export default function Account(props) {
     return <div className="account">
         <h2>{props.name}</h2>
-        <h1><Balance address={props.address} factor={1_000_000_000} decimals={0}/></h1>
-        <h2><Balance address={props.address}/></h2>
+        <h1><Balance address={props.address} factor={1_000_000_000} decimals={0}/>€</h1>
+        <h2 className="yellow">
+            <img src="/banano_icon.svg" className="icon"/>
+            <Balance address={props.address}/>
+        </h2>
     </div>
 }
