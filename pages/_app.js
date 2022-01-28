124aaba50b8fd340324a8a525554adb996637f3e
Jakob Schödl <jschoedl04@gmail.com>
2022-01-28 17:45:37 +0100

    add Banano logo

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,13 @@ export default function MyApp({Component, pageProps}) {
     return (
         <>
             <Head>
-                <title>React App</title>
+                <title>Banopoly</title>
                 <meta charSet="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <meta name="theme-color" content="#000000"/>
                 <meta
                     name="description"
-                    content="Web site created using create-react-app"
+                    content="Play MONOPOLY using the Banano cryptocurrency."
                 />
             </Head>
             <Component {...pageProps} />
