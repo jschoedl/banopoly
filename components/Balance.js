620816394123b160978c66f9a5f808352264ee58
Jakob Schödl <jschoedl04@gmail.com>
2022-01-29 14:19:42 +0100

    animate Balance change

--- components/Balance.js
+++ components/Balance.js
@@ -1,5 +1,6 @@
 import {Component} from "react";
 import bananojs from "@bananocoin/bananojs";
+import {sleep} from "../lib/helpers";
 
 bananojs.setBananodeApiUrl('https://kaliumapi.appditto.com/api');
 
@@ -34,6 +35,31 @@ class Balance extends Component {
         }
     }
 
+    async animateBalance(start, end) {
+        let diff = (end - start) / 10;
+        let current = start;
+        while (current < end) {
+            await sleep(20);
+            console.log(current)
+            current += diff;
+            diff *= 0.9;
+            this.setState({
+                formattedBalance: (current * this.factor || 0).toLocaleString(undefined, {
+                        maximumFractionDigits: this.decimals,
+                        minimumFractionDigits: this.decimals,
+                    },
+                ),
+            })
+        }
+        this.setState({
+            formattedBalance: (end * this.factor || 0).toLocaleString(undefined, {
+                    maximumFractionDigits: this.decimals,
+                    minimumFractionDigits: this.decimals,
+                },
+            ),
+        })
+    }
+
     updateBalance() {
         async function fetchBalance(address) {
             return bananojs.getAccountInfo(address);
@@ -42,15 +68,18 @@ class Balance extends Component {
         fetchBalance(this.state.address).then((res) => {
             if (res.error && res.error !== "Account not found") {
                 console.log(res.error);
-            } else
+            } else {
+                this.animateBalance(this.state.balance || 0, res.balance || 0).then(r => {
+                });
                 this.setState({
                     balance: res.balance,
                     formattedBalance: (res.balance * this.factor || 0).toLocaleString(undefined, {
                             maximumFractionDigits: this.decimals,
                             minimumFractionDigits: this.decimals,
                         },
-                    )
+                    ),
                 });
+            }
         });
 
         if (this.update) {
